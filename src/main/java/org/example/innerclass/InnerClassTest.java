3efda53af0cd1c1143c6fca8ebef8b22ac125715
Administrator <1253259103@qq.com>
2021-04-22 18:03:41 +0800

    leetcode、while(1)

--- src/main/java/org/example/innerclass/InnerClassTest.java
+++ src/main/java/org/example/innerclass/InnerClassTest.java
@@ -8,4 +8,38 @@ package org.example.innerclass;
  *  静态内部类
  */
 public class InnerClassTest {
+
+    /**
+     * 成员内部类
+     */
+    class MemberInnerClass {
+
+    }
+
+    /**
+     * 方法内部类
+     */
+    void test(){
+        class MethodInnerClass {
+
+        }
+    }
+
+    /**
+     * 匿名内部类
+     */
+
+
+
+    /**
+     * 静态内部类
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+            System.out.println("1");
+            Thread.sleep(1000);
+        }
+    }
+
 }
