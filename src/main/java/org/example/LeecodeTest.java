92f4e0f67d53430ebd8dc4cc21fbd2666e11ad5f
Administrator <1253259103@qq.com>
2021-04-28 23:08:40 +0800

    有效的括号

--- src/main/java/org/example/LeecodeTest.java
+++ src/main/java/org/example/LeecodeTest.java
@@ -1,29 +1,56 @@
 package org.example;
 
+import java.util.*;
+
 public class LeecodeTest {
     public static void main(String[] args) {
         //System.out.println((-205)%10);
-        String a = "";
+        String a = "a{s}d[f]a(sd)f";
+        System.out.println(a.toCharArray()[2]);
+        System.out.println(a.toCharArray().toString());
+        for (char c : a.toCharArray()) {
+            System.out.print(c);
+        }
+        List list = Arrays.asList(a.toCharArray());
+        for (Object o : list) {
+            System.out.print(o.toString());
+        }
+        Collections.sort(list);
+        for (Object o : list) {
+            System.out.print(o.toString());
+        }
+
+
+        //System.out.println(a.replace("()","o"));
         String b[] = a.split("as");
         for (String s : b) {
-            System.out.println(s);
+           // System.out.println(s);
         }
-        System.out.println(a.startsWith("as", 2));
-        int x;
-        x=1;
-        for (int i = 0; i < 5; i++) {
-            x = 9;
-            for (int j = 0; j < 3; j++) {
-                x = 8;
+        //System.out.println(a.startsWith("as", 2));
+        System.out.println(isValid("()"));
+
+
+
+    }
+
+    public static boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        char[] chars2 = new char[chars.length];
+        Stack stack1 = new Stack();
+
+        char[] chars3 = new char[chars.length];
+        Map<Character,Character> map = new HashMap();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        for (int i = 0 ; i < chars.length ; i++) {
+            if (chars[i] == '(' || chars[i] == '{' || chars[i] == '['){
+                stack1.push(chars[i]);
+            }else if (chars[i] == ')' || chars[i] == '}' || chars[i] == ']'){
+                if (!stack1.pop().equals(map.get(chars[i]))) return false;
             }
         }
-        System.out.println(x);
-        System.out.println(a.substring(0,0));
-
-        char[] chara = new char[]{'q','w'};
-        System.out.println(String.valueOf(chara, 0, 1));
-
-
+        return true;
     }
-    }
+}
 
---
+++ src/main/java/org/example/leetcode/editor/cn/[20]有效的括号.java
@@ -0,0 +1,91 @@
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。 
+//
+// 有效字符串需满足： 
+//
+// 
+// 左括号必须用相同类型的右括号闭合。 
+// 左括号必须以正确的顺序闭合。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "()"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "()[]{}"
+//输出：true
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "(]"
+//输出：false
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "([)]"
+//输出：false
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：s = "{[]}"
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 104 
+// s 仅由括号 '()[]{}' 组成 
+// 
+// Related Topics 栈 字符串 
+// 👍 2370 👎 0
+
+package org.example.leetcode.editor.cn;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack stack = new Stack();
+        Map<Character,Character> map = new HashMap();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        boolean temp =false;
+        int num =0;
+        for (int i = 0 ; i < chars.length ; i++) {
+            if (chars[i] == '(' || chars[i] == '{' || chars[i] == '['){
+                num += 1;
+                stack.push(chars[i]);
+            }else if (chars[i] == ')' || chars[i] == '}' || chars[i] == ']'){
+                num -= 1;
+                temp = true;
+                if (stack.isEmpty()) return false;
+                if (!stack.pop().equals(map.get(chars[i]))) return false;
+            }
+        }
+        if (!temp) return false;
+        if (num !=0) return false;
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
