c06199f1f3042b7330bd20adfda60be7a29e0bb6
Administrator <1253259103@qq.com>
2021-05-13 17:24:11 +0800

    集合

--- src/main/java/org/example/collection/CollectionDemo.java
+++ src/main/java/org/example/collection/CollectionDemo.java
@@ -1,4 +1,11 @@
 package org.example.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class CollectionDemo {
+    public static void main(String[] args) {
+        Collection collection = new ArrayList();
+        System.out.println(collection.getClass());
+    }
 }
