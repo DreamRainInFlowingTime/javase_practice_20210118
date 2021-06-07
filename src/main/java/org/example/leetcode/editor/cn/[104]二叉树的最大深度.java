b6525e027b39e7fad66e86f903f3aa66daea8e01
kanjon <1253259103@qq.com>
2021-06-08 00:36:18 +0800

    二叉树的最小深度

--- src/main/java/org/example/leetcode/editor/cn/[104]二叉树的最大深度.java
+++ src/main/java/org/example/leetcode/editor/cn/[104]二叉树的最大深度.java
@@ -35,31 +35,32 @@
  * }
  * }
  */
-class Solution {
+class Solution24 {
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
     }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
 
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 //leetcode submit region end(Prohibit modification and deletion)
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,13 @@
+import org.example.gc.LocalVarGCTest;
+import org.example.inheritance.Dad;
+
 import java.awt.print.PrinterGraphics;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Main {
     int x= 1;
+    Dad dad = new Dad();
     public static void main(String[] args) {
         int x = 2;
         System.out.println("args = " + Arrays.deepToString(args));
@@ -16,12 +20,13 @@ public class Main {
         System.out.println(integer);
         System.out.println("珊电动阀\r");
         int[][] aa = new int[3][];
-        System.out.println(aa[1].length);
         //byte x = 2+aa;
         byte by = -1;
         System.out.println(by & 1);
         char b = '珊';
         System.out.println(Arrays.toString(new String[]{Character.toString(b)}));
+        new Dad();
+        System.out.println("----");
 
     }
 
